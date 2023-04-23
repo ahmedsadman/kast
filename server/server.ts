c2085cf13cdf32c2d089df71b25606c101c84e5d
Ahmed Sadman <ahmedsadman.211@gmail.com>
2023-04-24 03:24:10 +0600

    endpoint to get user info

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import rooms from './rooms';
 
 import EventsHander from './eventsHandler';
 
@@ -21,6 +22,40 @@ const io = new Server(server, {
 
 app.get('/status', (_req, res) => {
   res.send({ status: 'OK' });
+});
+
+app.get('/user/:userId', async (req, res) => {
+  const { userId } = req.params;
+  let callCount = 0;
+
+  const userPromise = new Promise((resolve) => {
+    /* When a new user requests info of itself instantly after joining, 
+    room might not be assigned. Thus, the waiting.
+    There are better ways of doing this, like breaking down the socket events. But
+    let's skip those for now
+
+    TODO: Explore cleaner options like breaking down socket events (joinRequestInit, joinRequestComplete)
+    */
+    const interval = setInterval(() => {
+      console.log('assigned room interval check...');
+      const user = rooms.getUser(userId);
+
+      if (callCount === 20) {
+        clearInterval(interval);
+        resolve(null);
+      }
+
+      if (user) {
+        clearInterval(interval);
+        resolve(user);
+      }
+
+      callCount += 1;
+    }, 200);
+  });
+
+  const user = await userPromise;
+  res.send({ user });
 });
 
 const eventsHandler = new EventsHander(io);
