e7c46de272eb0eb130f58b48b9fe601e317d350b
Ahmed Sadman <ahmedsadman.211@gmail.com>
2023-04-24 19:28:57 +0600

    remove polling from server

--- server/server.ts
+++ server/server.ts
@@ -26,35 +26,7 @@ app.get('/status', (_req, res) => {
 
 app.get('/user/:userId', async (req, res) => {
   const { userId } = req.params;
-  let callCount = 0;
-
-  const userPromise = new Promise((resolve) => {
-    /* When a new user requests info of itself instantly after joining, 
-    room might not be assigned. Thus, the waiting.
-    There are better ways of doing this, like breaking down the socket events. But
-    let's skip those for now
-
-    TODO: Explore cleaner options like breaking down socket events (joinRequestInit, joinRequestComplete)
-    */
-    const interval = setInterval(() => {
-      console.log('assigned room interval check...');
-      const user = rooms.getUser(userId);
-
-      if (callCount === 20) {
-        clearInterval(interval);
-        resolve(null);
-      }
-
-      if (user) {
-        clearInterval(interval);
-        resolve(user);
-      }
-
-      callCount += 1;
-    }, 200);
-  });
-
-  const user = await userPromise;
+  const user = rooms.getUser(userId);
   res.send({ user });
 });
 
