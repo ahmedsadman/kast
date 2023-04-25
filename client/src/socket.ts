395bd4b5fa61afd4582576c731181642309e55d5
Ahmed Sadman <ahmedsadman.211@gmail.com>
2023-04-26 02:07:05 +0600

    read server from env

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:3005', { path: '/socket' });
+export const socket = io(import.meta.env.VITE_SERVER_BASE_URL, { path: '/socket' });
