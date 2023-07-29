daf8afa39c8d2570897babe65b0e19a34415b90f
Ahmed Sadman <ahmedsadman.211@gmail.com>
2023-07-30 02:45:21 +0600

    handle reaction message passing from sockets

--- client/src/ChatSection/Reactions/ReactionsBar.tsx
+++ client/src/ChatSection/Reactions/ReactionsBar.tsx
@@ -1,12 +1,19 @@
 import { Box, HStack } from '@chakra-ui/react';
+import { socket } from '../../socket';
 
 function ReactionBar() {
   const emojis = ['😆', '🤮', '😍', '🔥', '😭'];
 
+  const handleClick = (emoji: string) => {
+    socket.emit('newReaction', {
+      emoji,
+    });
+  };
+
   return (
     <HStack justifyContent="space-around">
       {emojis.map((emoji) => (
-        <Box cursor="pointer" key={emoji} fontSize="1.5em">
+        <Box cursor="pointer" key={emoji} fontSize="1.5em" onClick={() => handleClick(emoji)}>
           {emoji}
         </Box>
       ))}
